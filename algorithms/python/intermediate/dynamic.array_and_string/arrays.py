22e6475a4ed35683214e6d0364c3908a29bffc0c
Truc Ly <truclybk.cs@gmail.com>
2021-11-11 23:21:11 +0700

    array

---
+++ algorithms/python/intermediate/dynamic.array_and_string/arrays.py
@@ -0,0 +1,5 @@
+na, nb = map(int, input().split())
+k, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+print('YES') if a[k - 1] < b[nb - m] else print('NO')
